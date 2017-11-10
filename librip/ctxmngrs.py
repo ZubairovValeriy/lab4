76d229207325101b242f459d3ae7bb36e672effb
ZubairovValeriy <ValeriyIU5-12@yandex.ru>
2017-11-10 14:37:36 +0300

    lab4ready

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 # Здесь необходимо реализовать
 # контекстный менеджер timer
 # Он не принимает аргументов, после выполнения блока он должен вывести время выполнения в секундах
@@ -6,3 +7,11 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+
+class timer():
+    def __enter__(self):
+        self.start = datetime.today()
+
+    def __exit__(self, exp_type, exp_value, exp_tr):
+        print(datetime.today() - self.start)
--- librip/gens.py
+++ librip/gens.py
@@ -2,23 +2,27 @@ import random
 
 
 # Генератор вычленения полей из массива словарей
-# Пример:
-# goods = [
-#    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-#    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
-# ]
-# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
-# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
-
 def field(items, *args):
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+
+    if len(args) == 1:
+        for it in items:
+            for key in args:
+                elem = it.get(key)
+                if elem is not None:
+                    yield elem
+    else:
+        for it in items:
+            res_dict = {}
+            for key in args:
+                elem = it.get(key)
+                if elem is not None:
+                    res_dict[key] = elem
+            if len(res_dict)>0:
+                yield res_dict
 
 
 # Генератор списка случайных чисел
-# Пример:
-# gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
-# Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
-    # Необходимо реализовать генератор
+    for i in range(num_count):
+        yield random.randint(begin, end)
